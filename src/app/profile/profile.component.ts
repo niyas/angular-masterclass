b34f2675eec31cf2841e36a7f3ab94bddb724834
Mohammed Niyas <Mohammedniyas@MSSPLLAP066.meritgroup.com>
2018-05-28 08:32:47 +0100

    Routing parameter example

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap
+      .subscribe(params => {
+        let id = +params.get('id');
+        console.log(id);
+      });
   }
 
 }
